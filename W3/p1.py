5126ccce35bd1fb594b4160057d0c324d19e9a50
Michael Zhang <michaelzhang@Michaels-MacBook-Pro.local>
2020-04-09 15:20:36 -0700

    mz - added problem 1

--- W3/p1.py
+++ W3/p1.py
@@ -1,21 +1,4 @@
-# Exercises for Weeks 3
-
-## Problem 1
+'''
 Write a program which takes 5 strings as input and appends them in that order
 to a list *l*. Then, concatenate them into a single string *s* 
-
-
-## Problem 2
-
-
-
-## Problem 3
-
-
-
-## Problem 4
-
-
-
-## Problem 5
-
+'''
