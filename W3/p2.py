17a4840b2a71cf3ac275a3e1bd22b5b6dfbe1a56
Michael Zhang <michaelzhang@Michaels-MacBook-Pro.local>
2020-04-09 15:58:32 -0700

    mz - added problems

---
+++ W3/p2.py
@@ -0,0 +1,13 @@
+'''
+Consider a vending machine represented as a dictionary with candy items (represented by their names)
+mapped to prices. The names of the candy items are also stored in *names*, a list of strings. 
+Write a program which outputs all candy names and their prices in the following form. 
+
+* The price of [*name*] is [*price*]
+
+As the vending machine was not profitable enough, the administrators decided to up the prices
+by 150%. Implement these changes to the dictionary mapping *names* to *prices*.
+
+Test your code in both of the above questions by creating your own dictionaries and names.
+
+'''
