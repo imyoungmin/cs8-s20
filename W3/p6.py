2071a6dac8da139019f262c19d1ef9adb4605afa
Michael Zhang <michaelzhang@Michaels-MacBook-Pro.local>
2020-04-10 14:39:53 -0700

    added p6

---
+++ W3/p6.py
@@ -0,0 +1,10 @@
+'''
+Write a program which takes 5 strings as inputs and appends them to a list l. Swap each element in l with its mirror - that is, for some element at index i, swap l[i] with l[4-i] if i < 3.
+
+For instance, if l = ['Alex', 'Bob', 'Charlie', 'David', 'Ethan'], then after swapping, l should read
+['Ethan', 'David', 'Charlie', 'Bob', 'Alex']
+
+Finally, finish the program by creating a dictionary mapping each name to its length.
+'''
+
+# Solution here
