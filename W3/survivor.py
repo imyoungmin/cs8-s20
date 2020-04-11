e7f4b63447b09b8664685d6e37949b82034bba62
Alex Mei <34776716+alexmeigz@users.noreply.github.com>
2020-04-10 18:43:26 -0700

    starter code for problem 7

---
+++ W3/survivor.py
@@ -0,0 +1,20 @@
+HIDDEN_MESSAGE = "It’s like the Black Widow Brigade — we’re spinning them, \
+spinning them, and spinning them until they don’t know which way is up and \
+then we’re devouring them. One at a time."
+
+HIDDEN_MESSAGE_2 = "The king may be powerful, but in the end, the king does \
+whatever the queen wants. The Queen has the real power."
+
+tokens = {"jar(s) of peanut butter": 1,
+          "hidden immunity idol(s)": 3,
+          "tarp(s)": 5,
+          "challenge advantage(s)" : 4,
+          "bag(s) of rice" : 5,
+          "dozen(s) of pastries" : 3,
+          "set(s) of pillows and blankets": 2,
+}
+
+if __name__ == '__main__':
+    print(HIDDEN_MESSAGE)
+    print(HIDDEN_MESSAGE_2)
+    #Bonus Coconuts for those who identify the person who said the quotes
